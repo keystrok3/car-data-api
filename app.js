45825431567815469209ae4315ae115bf3188bbd
Josiah <jwalutsachi4@gmail.com>
2020-11-20 20:24:52 +0300

    On branch master  Changes to be committed: 	modified:   app.js Added new routes

--- app.js
+++ app.js
@@ -14,4 +14,42 @@ app.get('/allcars', async (req, res) => {
     }
 });
 
+// Find all cars going by a certain name
+app.get('/findacar/:carname', async (req, res) => {
+    let name = req.params.carname;
+    try {
+        let data = await db.find_a_car(name);
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+// Find all cars from a specific country
+app.get('/findbycountry/:origin', async (req, res) => {
+    let origin = req.params.origin;
+    try {
+        let data = await db.findByOrigin(origin);
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+// Find proportion of cars from a particular country
+app.get('/originproportion/:origin', async (req, res) => {
+    let origin = req.params.origin;
+    try {
+        let total = await db.getTotal();
+        let totalFromOrigin = await db.getTotalFromCountry(origin);
+        console.log((totalFromOrigin[0].totalFromOrigin / total[0].totalCars)*100);
+        res.send();
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+})
+
 app.listen(3000);
